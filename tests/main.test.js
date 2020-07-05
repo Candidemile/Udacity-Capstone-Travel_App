aaf45e0847a683f0bdc362eb00e8657f492d6cf0
Candid_emile <doctor_net@mail.ru>
2020-07-05 16:43:34 +0300

    feat: add countdown function

--- tests/main.test.js
+++ tests/main.test.js
@@ -6,19 +6,30 @@ import { fetchGeonamesApi } from '../src/server/geonamesAPI';
 import { TestScheduler } from 'jest';
 
 // ** TESTING ** //
-describe('Client side code', () => {
-    test('Test Geonames API', () => {
-        let test = fetchGeonamesApi();
-        console.log(test);
-        let testOk = test.then((res) => {
-            console.log(res);
-            return res.ok;
-        });
-        console.log(testOk);
-        expect(testOk).toBeTruthy();
-    });
-});
+// describe('Client side code', () => {
+//     test('Test Geonames API', () => {
+//         let test = fetchGeonamesApi();
+//         console.log(test);
+//         let testOk = test.then((res) => {
+//             console.log(res);
+//             return res.ok;
+//         });
+//         console.log(testOk);
+//         expect(testOk).toBeTruthy();
+//     });
+// });
 
 describe('Server side code', () => {
     test('', () => {});
+});
+
+function test() {
+    console.log('test');
+}
+
+describe('test code', () => {
+    it('test function', () => {
+        console.log(typeof test);
+        expect(typeof test).toBe('function');
+    });
 });
