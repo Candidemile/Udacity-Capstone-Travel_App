788240f41904ecaf8cedbeefd349aba7ff79f272
Candid_emile <doctor_net@mail.ru>
2020-07-12 23:17:11 +0300

    feat: add form

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import './styles/normalize.scss';
+import './styles/blog-post.scss';
+// import './styles/style.scss';
+// import './styles/style-content.scss';
+// import './styles/style-footer.scss';
+// import './styles/style-header.scss';
+// import './styles/style-sidebar.scss';
+// import './styles/';
+// import './styles/';
