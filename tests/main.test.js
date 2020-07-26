3698e28865dc59486e8c5968bf300371f980ee54
Candid_emile <doctor_net@mail.ru>
2020-07-26 19:28:13 +0300

    test: add test

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,9 +1,23 @@
-// client .js files
+// @@ client side tests @@
+// import
+import { displayCovidLevel } from '../src/client/js/displayCovidLevel';
+// tests
+test('displayCovidLevel function tests', () => {
+    expect(displayCovidLevel('')).toEqual([ 'no-data', 0 ]);
+    expect(displayCovidLevel()).toEqual([ 'no-data', 0 ]);
+    expect(displayCovidLevel('low')).toEqual([ 'low', 20 ]);
+    expect(displayCovidLevel('moderate')).toEqual([ 'moderate', 40 ]);
+    expect(displayCovidLevel('high')).toEqual([ 'high', 60 ]);
+    expect(displayCovidLevel('very high')).toEqual([ 'very-high', 80 ]);
+    expect(displayCovidLevel('extreme')).toEqual([ 'extreme', 100 ]);
+    expect(displayCovidLevel('extreme')).not.toEqual([ 'no-data', 0 ]);
+    expect(displayCovidLevel('whatever went here')).not.toEqual([ 'low', 20 ]);
+});
 
 // server .js files
-import '@babel/polyfill';
-import { fetchGeonamesApi } from '../src/server/geonamesAPI';
-import { TestScheduler } from 'jest';
+// import '@babel/polyfill';
+// import { fetchGeonamesApi } from '../src/server/geonamesAPI';
+// import { TestScheduler } from 'jest';
 
 // ** TESTING ** //
 // describe('Client side code', () => {
@@ -19,17 +33,17 @@ import { TestScheduler } from 'jest';
 //     });
 // });
 
-describe('Server side code', () => {
-    test('', () => {});
-});
+// describe('Server side code', () => {
+//     test('', () => {});
+// });
 
-function test() {
-    console.log('test');
-}
+// function test() {
+//     console.log('test');
+// }
 
-describe('test code', () => {
-    it('test function', () => {
-        console.log(typeof test);
-        expect(typeof test).toBe('function');
-    });
-});
+// describe('test code', () => {
+//     it('test function', () => {
+//         console.log(typeof test);
+//         expect(typeof test).toBe('function');
+//     });
+// });
